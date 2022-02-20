b3c4acbadda91736e1838986cf6ec5ecd31443ea
Amin Noori <mohd.amin.noori@gmail.com>
2022-02-19 20:19:18 -0500

    this is a commit for new file addition
    This is the formation for description

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Some Description!
 
 I Keep my personal daily php practice here.
+
+## Subheader 
+
+This is to check the coonnection of github and my new set up.
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+function greet($name, $color){
+    echo "<p>Hi, my name is $name and my faviourt color is $color</p>";
+}
+greet('John', 'blue');
+greet('Jane', 'green');
+// arguments and parameters examples
+?>
